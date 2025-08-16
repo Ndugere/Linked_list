d76d09d361382ad62ad0c886c81c5e2b39b2888f
Ndugere <ndungudavid972@gmail.com>
2025-08-16 16:49:57 +0300

    Added the prepend method here

--- singlelinklist.py
+++ singlelinklist.py
@@ -4,19 +4,20 @@ class Node:
         self.next = None
     
 
-
 class LinkedList:
     def __init__(self):
         self.head = None
-
-
     
     def append(self, value):
         if not self.head:
             self.head = Node(value)
-            return
-        last = self.head
-        while last.next:
-            last = last.next
-        last.next = Node(value)
+        else:
+            last = self.head
+            while last.next:
+                last = last.next
+            last.next = Node(value)
         
+    def prepend(self, value):
+        first_node = Node(value)
+        first_node.next = self.head
+        self.head = first_node
