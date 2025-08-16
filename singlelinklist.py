545b144481fcb54110bab296cff915cb07d45c4d
Ndugere <ndungudavid972@gmail.com>
2025-08-16 21:08:39 +0300

    Made the insert menu

--- singlelinklist.py
+++ singlelinklist.py
@@ -21,3 +21,25 @@ class LinkedList:
         first_node = Node(value)
         first_node.next = self.head
         self.head = first_node
+    
+
+    def insert(self, value, index):
+        if index == 0:
+            self.prepend(value)
+            return
+        if not self.head:
+            raise ValueError("Index out of reach")
+        x = 0
+        last_seen = self.head
+        while x < index - 1:
+            if not last_seen.next:
+                raise ValueError("Index out of reach")
+            last_seen = last_seen.next
+            x = x+1
+        new_node = Node(value)
+
+        new_node.next = last_seen.next
+        last_seen.next = new_node
+
+            
+
