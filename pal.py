2b8e1a11f1c6929e2740e92f2ba9d790384b5e4d
Your Name <you@example.com>
2025-08-20 14:09:30 +0300

    Add palindrome checker that ignores non-alphanumeric characters and case

---
+++ pal.py
@@ -0,0 +1,15 @@
+def is_palindrome(s):
+    """
+    Checks if the given string is a palindrome, ignoring non-alphanumeric characters and case.
+    """
+    l, r = 0, len(s) - 1
+    while l < r:
+        while l < r and not s[l].isalnum():
+            l += 1
+        while l < r and not s[r].isalnum():
+            r -= 1
+        if s[l].lower() != s[r].lower():
+            return False
+        l += 1
+        r -= 1
+    return True
